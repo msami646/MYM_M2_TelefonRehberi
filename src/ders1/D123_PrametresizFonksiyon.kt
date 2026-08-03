f8a27f7d31575dff70122bb82999835ebcc2ce56
Mehmet Sami Aydın <msamiaydin1717@gamil.com>
2026-08-03 12:32:08 +0300

    Ders1: Proje oluşturuldu

---
+++ src/ders1/D123_PrametresizFonksiyon.kt
@@ -0,0 +1,13 @@
+package ders1
+
+fun main() {
+    println("1.görev tamamlandı")
+    cizgiCek()
+
+    println("'.görev tamamlandı.")
+    cizgiCek()
+}
+
+fun cizgiCek(){
+    println("-----------------")
+}
